b7c4b74f17dad82db94256da145db121c89a11e8
Bart Kiers <bkiers@gmail.com>
2013-05-10 22:37:21 +0200

    Made addVariable(...) and addFunction(...) public.

---
+++ src/main/curta/Test.java
@@ -0,0 +1,11 @@
+package curta;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: bart
+ * Date: 1/3/13
+ * Time: 8:42 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Test {
+}
